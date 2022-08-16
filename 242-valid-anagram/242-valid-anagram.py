5fb4574e955a8f0afbeee28ffb09a3e458ed8417
rohan meruliya <90633430+rmeruliya@users.noreply.github.com>
2022-08-16 14:58:06 -0400

    Time: 69 ms (63.42%), Space: 14.4 MB (67.20%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -2,7 +2,16 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-        for i in s:
-            if s.count(i) != t.count(i):
+        
+        hash1, hash2 = {}, {}
+        
+        for i in range(len(s)):
+            hash1[s[i]] = 1 + hash1.get(s[i],0)
+            hash2[t[i]] = 1 + hash2.get(t[i],0)
+            
+        for i in hash1:
+            if hash1[i] != hash2.get(i,0):
                 return False
         return True
+            
+            
